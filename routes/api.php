f31b84ce84bd49e2e94830d2f63f3df1b1a3d5b6
naotarian <naotarian19@gmail.com>
2022-06-03 08:28:34 +0000

    イベント登録まで

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(EventController::class)->group(function(){
+    Route::post('/create_event', 'create_event');
 });
