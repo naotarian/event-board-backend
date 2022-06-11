3e5fcbef934d7061a48d7091ce7ea14d2538a5da
naotarian <naotarian19@gmail.com>
2022-06-12 00:45:19 +0900

    メール認証

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 /*
 |--------------------------------------------------------------------------
 | API Routes
