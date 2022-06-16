93a41a66f2badea9f68fcdec273b99aff3cb1538
naotarian <naotarian19@gmail.com>
2022-06-16 06:45:02 +0000

    test practice

--- tests/Unit/Event/GetEventTest.php
+++ tests/Unit/Event/GetEventTest.php
@@ -2,26 +2,28 @@
 
 namespace Tests\Unit\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 // use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Event;
 
 class GetEventTest extends TestCase
 {
     use RefreshDatabase;
+    use DatabaseTransactions;
     private $accessToken = null;
 
-    protected function setUp(): Void // ※ Voidが必要
+    protected function setUp(): Void
     {
         // 必ずparent::setUp()を呼び出す
         parent::setUp(); 
         $user = User::factory()->create();
-
         $response = $this->actingAs($user)
-                         ->withSession(['banned' => false])
-                         ->get('/');
+        ->withSession(['banned' => false])
+        ->get('/');
     }
     /**
      * A basic test example.
@@ -30,6 +32,8 @@ class GetEventTest extends TestCase
      */
     public function test_get_events()
     {
+        // $event = Event::factory()->create();
+        // \Log::info(json_decode($event, true));
         $response = $this->get('/api/get_events');
         $response->dump();
         $response->assertStatus(200);
